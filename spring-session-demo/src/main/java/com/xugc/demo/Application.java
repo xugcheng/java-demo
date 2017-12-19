e6855a283db7501ebe616b538975b8858e16b0d7
xuguocheng <xuguocheng@icomwell.com>
2017-12-19 16:23:36 +0800

    session过期时间

--- spring-session-demo/src/main/java/com/xugc/demo/Application.java
+++ spring-session-demo/src/main/java/com/xugc/demo/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
-@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 5 * 60 * 1000)
+@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 5 * 60 * 60)
 public class Application {
 
     public static void main(String[] args) {
